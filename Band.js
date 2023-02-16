e71427c0fe41e80d6150d84594e0742dc053800b
vee-el <veronicalennon@live.com>
2023-02-16 11:51:17 +0000

    updated db file and band.js

--- Band.js
+++ Band.js
@@ -1,7 +1,11 @@
 const {Sequelize, sequelize} = require('./db');
+const { DataTypes } = require('sequelize');
 
 // TODO - define the Band model
-let Band;
+let Band = sequelize.define('Band', {
+    name: DataTypes.STRING,
+    genre: DataTypes.STRING
+});
 
 module.exports = {
     Band
--- db.js
+++ db.js
@@ -3,6 +3,11 @@ const { Sequelize, Model } = require('sequelize');
 
 // TODO - create the new sequelize connection
 
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'db.sqlite')
+})
+
 module.exports = {
     sequelize,
     Sequelize
